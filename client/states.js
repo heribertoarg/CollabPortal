0259cba582d975774c8c66afde75400d226f5628
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-29 08:01:58 -0700

    [Studio] Delete screen "app.viewissues"

--- client/states.js
+++ client/states.js
@@ -51,14 +51,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.viewissues', {
-        views: {
-            app: {
-                controller: 'app_viewissues',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.viewissues');
-                }
-            }
-        }
     });
 });
