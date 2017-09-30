0dd4d22c59d11e92ba22a1a5ebe205a87416ccb9
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-29 18:49:32 -0700

    [Studio] Modified screen "SearchCriteria"

--- client/controllers/app/searchcriteria.js
+++ client/controllers/app/searchcriteria.js
@@ -2,6 +2,17 @@ angular.module('app').controller('app_searchcriteria', app_searchcriteria);
 function app_searchcriteria($scope, $rootScope, $http, app) {
     'use strict';
     app.init($scope);
+
+    $scope.getdynpage = function(){
+        $http.get("http://localhost:44330/api/ConfigurationApi")
+        .then(function successCallback(response) {
+            $rootScope.dynPage = response.data;
+            //app.go('app.searchresults'); 
+        }, function errorCallback(response) {
+            return response;
+        });
+    }
+
     
     $rootScope.dynPageName = "OrderView";
     
@@ -555,13 +566,5 @@ function app_searchcriteria($scope, $rootScope, $http, app) {
         });
     }
     
-    $scope.getdynpage = function(){
-        $http.get("http://localhost:44330/api/ConfigurationApi")
-        .then(function successCallback(response) {
-            $rootScope.dynPage = response.data;
-            //app.go('app.searchresults'); 
-        }, function errorCallback(response) {
-            return response;
-        });
-    }
+
 }
