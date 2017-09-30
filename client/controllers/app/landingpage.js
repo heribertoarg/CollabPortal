690625bb1cac11fc95039bf7df58436ebc10d4b5
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-29 19:07:19 -0700

    [Studio] Add screen "LandingPage"

---
+++ client/controllers/app/landingpage.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_landingpage', app_landingpage);
+function app_landingpage($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.landingpage', {
+        views: {
+            app: {
+                controller: 'app_landingpage',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.landingpage');
+                }
+            }
+        }
     });
 });
