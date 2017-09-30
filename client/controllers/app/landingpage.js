8c27fc81b0bdd43c1380cddf83f2a18bdb383793
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-29 19:23:37 -0700

    [Studio] Modified screen "LandingPage"

--- client/controllers/app/landingpage.js
+++ client/controllers/app/landingpage.js
@@ -5,7 +5,19 @@ function app_landingpage($scope, $rootScope, $http, app) {
     
     $scope.getdynpage = function(){
         
+        
+        
         debugger;
+        $scope.search = function(){
+        $http.post("http://localhost:61454/api/values",$scope.searchArray)
+            .then(function successCallback(response) {
+                $rootScope.searchResults = response.data;
+                app.go('app.searchresults'); 
+            }, function errorCallback(response) {
+                return response;
+            });
+        }
+        
         
         $http.get("http://localhost:44330/api/configuration/dynpages/OrderView")
         .then(function successCallback(response) {
