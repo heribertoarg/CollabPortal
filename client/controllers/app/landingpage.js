b6567783a2bd632183eee8906256dd47f3297059
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-29 20:06:15 -0700

    [Studio] Modified screen "SearchCriteria", Modified screen "LandingPage"

--- client/controllers/app/landingpage.js
+++ client/controllers/app/landingpage.js
@@ -6,7 +6,7 @@ function app_landingpage($scope, $rootScope, $http, app) {
     $scope.getdynpage = function(){
         $http.get("https://hkdnte214.asia.ad.flextronics.com:2872/api/configuration/dynpages/OrderView")
         .then(function successCallback(response) {
-            //$rootScope.dynPage = response.data;
+            $rootScope.dynPage = response.data;
             app.go('app.searchcriteria'); 
         }, function errorCallback(response) {
             return response;
