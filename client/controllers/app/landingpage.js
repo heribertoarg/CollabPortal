9b7bddfbe1033dbca5dcdd4652eb79c7ed7614b5
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-29 19:16:52 -0700

    [Studio] Modified screen "LandingPage"

--- client/controllers/app/landingpage.js
+++ client/controllers/app/landingpage.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('app_landingpage', app_landingpage);
-function app_landingpage($scope, app) {
+function app_landingpage($scope, $rootScope, $http, app) {
     'use strict';
     app.init($scope);
     
