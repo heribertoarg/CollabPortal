68db4781871b8b56a4bcf11933db9e9e40d8f327
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-10-02 08:20:20 -0700

    [Studio] Delete screen "app.sidemenuclassic"

--- client/states.js
+++ client/states.js
@@ -51,14 +51,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.sidemenuclassic', {
-        views: {
-            app: {
-                controller: 'app_sidemenuclassic',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.sidemenuclassic');
-                }
-            }
-        }
     });
 });
