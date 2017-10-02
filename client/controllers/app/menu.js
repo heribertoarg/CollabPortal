95a0e1b3d5d44b4dab9a54a6779c719f2379aeec
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-10-02 08:55:06 -0700

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -15,4 +15,14 @@ function app_menu($scope, app) {
         }
       ]
     }
+    
+    $scope.getdynpage = function(dynPageName){
+        $http.get("https://hkdnte214.asia.ad.flextronics.com:2872/api/configuration/dynpages/" + dynPageName)
+        .then(function successCallback(response) {
+            $rootScope.dynPage = response.data;
+            app.go('app.searchcriteria'); 
+        }, function errorCallback(response) {
+            return response;
+        });
+    }
 }
