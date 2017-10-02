ac2e6a2e5275bdd6c34ac4731ee5beb487cdc712
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-10-02 07:47:52 -0700

    [Studio] Modified screen "LandingPage"

--- client/controllers/app/landingpage.js
+++ client/controllers/app/landingpage.js
@@ -3,8 +3,9 @@ function app_landingpage($scope, $rootScope, $http, app) {
     'use strict';
     app.init($scope);
     
-    $scope.getdynpage = function(){
-        $http.get("https://hkdnte214.asia.ad.flextronics.com:2872/api/configuration/dynpages/OrderView")
+    $scope.getdynpage = function(dynPageName){
+        //$http.get("https://hkdnte214.asia.ad.flextronics.com:2872/api/configuration/dynpages/OrderView")
+        $http.get("https://hkdnte214.asia.ad.flextronics.com:2872/api/configuration/dynpages/"+dynPageName)
         .then(function successCallback(response) {
             $rootScope.dynPage = response.data;
             app.go('app.searchcriteria'); 
