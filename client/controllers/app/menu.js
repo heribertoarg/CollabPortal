52c9debaf8740865237f4484f78a2be3afe2c1ce
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-10-02 07:33:05 -0700

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -3,35 +3,35 @@ function app_menu($scope, app) {
     'use strict';
     app.init($scope);
     
-    $scope.menu{ "sideMenuGroups": [
-    {
-      "groupName": "Menu Group - 1",
-      "sideMenuItems": [
+    $scope.menus = { "sideMenuGroups": [
         {
-          "itemName": "Group 1 - Menu Option 1"
+          "groupName": "Menu Group - 1",
+          "sideMenuItems": [
+            {
+              "itemName": "Group 1 - Menu Option 1"
+            },
+            {
+              "itemName": "Group 1 - Menu Option 2"
+            },
+            {
+              "itemName": "Group 1 - Menu Option 3"
+            },
+            {
+              "itemName": "Group 1 - Menu Option 4"
+            }
+          ]
         },
         {
-          "itemName": "Group 1 - Menu Option 2"
-        },
-        {
-          "itemName": "Group 1 - Menu Option 3"
-        },
-        {
-          "itemName": "Group 1 - Menu Option 4"
-        }
-      ]
-    },
-    {
-      "groupName": "Menu Group - 2",
-      "sideMenuItems": [
-        {
-          "itemName": "Group 2 - Menu Option 1"
-        },
-        {
-          "itemName": "Group 2 - Menu Option 2"
+          "groupName": "Menu Group - 2",
+          "sideMenuItems": [
+            {
+              "itemName": "Group 2 - Menu Option 1"
+            },
+            {
+              "itemName": "Group 2 - Menu Option 2"
+            }
+          ]
         }
       ]
     }
-  ]
-}
 }
