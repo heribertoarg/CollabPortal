482d675c1ef2628e593180749964675ccc15f736
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-10-02 08:55:53 -0700

    [Studio] Modified screen "LandingPage"

--- client/controllers/app/landingpage.js
+++ client/controllers/app/landingpage.js
@@ -3,13 +3,4 @@ function app_landingpage($scope, $rootScope, $http, app) {
     'use strict';
     app.init($scope);
     
-    $scope.getdynpage = function(dynPageName){
-        $http.get("https://hkdnte214.asia.ad.flextronics.com:2872/api/configuration/dynpages/" + dynPageName)
-        .then(function successCallback(response) {
-            $rootScope.dynPage = response.data;
-            app.go('app.searchcriteria'); 
-        }, function errorCallback(response) {
-            return response;
-        });
-    }
 }
