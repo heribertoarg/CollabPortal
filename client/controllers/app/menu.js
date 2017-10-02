3e3bd88dfed362d261009d733a06f256375b6072
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-10-02 08:46:53 -0700

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -10,26 +10,6 @@ function app_menu($scope, app) {
             {
               "itemName": "Order View",
               "dynPageName" : "OrderView"
-            },
-            {
-              "itemName": "Group 1 - Menu Option 2"
-            },
-            {
-              "itemName": "Group 1 - Menu Option 3"
-            },
-            {
-              "itemName": "Group 1 - Menu Option 4"
-            }
-          ]
-        },
-        {
-          "groupName": "Menu Group - 2",
-          "sideMenuItems": [
-            {
-              "itemName": "Group 2 - Menu Option 1"
-            },
-            {
-              "itemName": "Group 2 - Menu Option 2"
             }
           ]
         }
