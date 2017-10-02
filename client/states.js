cbbdf35506d183c2a39f32e3c0a1fad36547c82b
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-10-02 10:14:28 -0700

    [Studio] Delete screen "app.detailsviewconfident"

--- client/states.js
+++ client/states.js
@@ -51,14 +51,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.detailsviewconfident', {
-        views: {
-            app: {
-                controller: 'app_detailsviewconfident',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.detailsviewconfident');
-                }
-            }
-        }
     });
 });
