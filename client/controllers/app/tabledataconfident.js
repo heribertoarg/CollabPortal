7cf4134d771f42c0b6e9c0220e83214dc818bfb6
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-11-09 09:30:00 -0800

    [Studio] Delete screen "app.tabledataconfident"

--- client/controllers/app/tabledataconfident.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('app_tabledataconfident', app_tabledataconfident);
-function app_tabledataconfident($scope, app) {
-    'use strict';
-    app.init($scope);
-}
--- client/states.js
+++ client/states.js
@@ -69,14 +69,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.tabledataconfident', {
-        views: {
-            app: {
-                controller: 'app_tabledataconfident',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.tabledataconfident');
-                }
-            }
-        }
     });
 });
