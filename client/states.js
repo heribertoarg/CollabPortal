84bfc67226e85cfd414128f92e94a154e0310bfe
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-11-09 09:28:08 -0800

    [Studio] Add screen "ListView-Confident"

--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.listviewconfident', {
+        views: {
+            app: {
+                controller: 'app_listviewconfident',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.listviewconfident');
+                }
+            }
+        }
     });
 });
